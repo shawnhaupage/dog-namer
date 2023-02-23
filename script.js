28b6a46d414d041db2a6586e7fa7455606ab244f
Shawn Haupage <111591051+shawnhaupage@users.noreply.github.com>
2023-02-23 10:09:22 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,72 +1,44 @@
+const createDogButton = document.getElementById('create-dog');
+const dogImage = document.getElementById('dog-image');
+const dogName = document.getElementById('dog-name');
+const genderButtons = document.querySelectorAll('.gender-btn');
+
 // Define arrays of pre-selected dog names
 const maleDogNames = ['Max','Charlie','Cooper','Milo','Buddy','Rocky','Bear','Teddy','Duke','Leo','Beau','Tucker','Oliver','Jack','Bentley','Ollie','Winston','Zeus','Toby','Loki','Jax','Finn','Blu','Bruno','Murphy','Louie','Moose','Gus','Koda','Hank','Apollo','Ace','Archie','Henry','Dexter','Maverick','Thor','Scout','Kobe'];
 const femaleDogNames = ['Luna','Bella','Daisy','Lucy','Lily','Zoe','Lola','Sadie','Bailey','Stella','Coco','Molly','Maggie','Penny','Nala','Rosie','Roxy','Ruby','Chloe','Sophie','Ellie','Willow','Piper','Millie','Gracie','Mia','Pepper','Nova','Callie','Winnie','Lulu','Hazel','Harley','Ginger','Olive','Abby','Charlie','Honey','Lexi','Athena','Riley','Izzy','Remi','Maya','Layla','Lady','Kona','Sasha','Poppy'];
 
-const dog = {
-  name: "",
-  image: "",
-};
-
-const createDogBtn = document.getElementById("create-dog");
-const saveDogBtn = document.getElementById("save-dog");
-const maleBtn = document.getElementById("male-btn");
-const femaleBtn = document.getElementById("female-btn");
-
-let gender = "male";
-
-// Set dog name and image based on selected gender
-function setDog() {
-  if (gender === "male") {
-    dog.name = maleNames[Math.floor(Math.random() * maleNames.length)];
-  } else {
-    dog.name = femaleNames[Math.floor(Math.random() * femaleNames.length)];
-  }
-  dog.image = "";
-  fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response => response.json())
-    .then(data => {
-      dog.image = data.message;
-      dogImage.src = dog.image;
-      document.getElementById("dog-name").textContent = dog.name;
-    })
-    .catch(error => console.log(error));
+// Get a random name from an array of dog names
+function getRandomName(gender) {
+  const names = gender === 'male' ? maleDogNames : femaleDogNames;
+  return names[Math.floor(Math.random() * names.length)];
 }
 
-// Update dog name and image on page load
-setDog();
+// Update the dog name on the page
+function updateDogName(gender) {
+  const name = getRandomName(gender);
+  dogName.textContent = name;
+}
 
-// Update dog name and image on "Create Dog" button click
-createDogBtn.addEventListener("click", () => {
-  setDog();
+createDogButton.addEventListener('click', () => {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+      dogImage.src = data.message;
+      dogImage.alt = 'Random Dog Image';
+      const gender = document.querySelector('.gender-btn.active').value; // Get the value of the active gender button
+      updateDogName(gender); // Update the dog name when a new dog image is created
+    })
+    .catch(error => console.error(error));
 });
 
-// Save dog image on "Save Dog" button click
-saveDogBtn.addEventListener("click", () => {
-  if (dog.image) {
-    const link = document.createElement("a");
-    link.href = dog.image;
-    link.download = dog.name + ".jpg";
-    link.click();
-    saveDogBtn.classList.remove("btn-transparent");
-    saveDogBtn.classList.add("btn-success");
-    setTimeout(() => {
-      saveDogBtn.classList.remove("btn-success");
-      saveDogBtn.classList.add("btn-transparent");
-    }, 1000);
-  }
+genderButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    genderButtons.forEach(btn => btn.classList.remove('active')); // Remove the active class from all gender buttons
+    button.classList
+    .add('active'); // Add the active class to the clicked gender button
+    const gender = button.value;
+    updateDogName(gender); // Update the dog name when the gender is changed
+  });
 });
 
-// Update dog name and gender on gender select button click
-maleBtn.addEventListener("click", () => {
-  gender = "male";
-  setDog();
-  maleBtn.classList.add("active");
-  femaleBtn.classList.remove("active");
-});
-
-femaleBtn.addEventListener("click", () => {
-  gender = "female";
-  setDog();
-  femaleBtn.classList.add("active");
-  maleBtn.classList.remove("active");
-});
+updateDogName(document.querySelector('.gender-btn.active').value); // Update the dog name when the page is loaded
